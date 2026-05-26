615e8ae8972c5f5299e22713fbe0d79752587927
maverick0403 <devjyoti.namata@gmail.com>
2026-05-26 12:16:04 +0100

    Update dataframe to include 'SEARCH_ON' column
    Modified the dataframe selection to include 'SEARCH_ON' column and updated the dataframe display.

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,8 +25,9 @@ cnx=st.connection("snowflake")
 session = cnx.session()
 
 #session = get_active_session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('fruit_name'))
-#st.dataframe(data=my_dataframe, use_container_width=True)
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('fruit_name'),col('SEARCH_ON'))
+st.dataframe(data=my_dataframe, use_container_width=True)
+st.stop()
 
 fruit_list = st.multiselect(
     "chose upto 5 fruits",
