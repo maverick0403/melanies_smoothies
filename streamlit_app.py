4acce3f6ec59eb1f83e54a9203a2724935a181db
maverick0403 <devjyoti.namata@gmail.com>
2026-05-26 12:24:44 +0100

    Implement fruit search and nutrition display
    Add search functionality for chosen fruit and display nutrition information.

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,6 +47,10 @@ if fruit_list:
     
     for fruits in fruit_list:
         ingredients_string += fruits + ' '
+
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+      
         st.subheader(fruits + ' Nutrition Information')
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon" + fruits)  
         st_df=st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
